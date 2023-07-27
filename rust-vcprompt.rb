2c8d51ad91a5b8abce05f34cb17449d847c80c08
Stefan Scherfke <stefan@sofa-rockers.org>
2023-07-27 21:34:52 +0200

    Update to 1.0.1

--- rust-vcprompt.rb
+++ rust-vcprompt.rb
@@ -1,8 +1,8 @@
 class RustVcprompt < Formula
   desc "Informative version control prompt for your shell"
   homepage "https://github.com/sscherfke/rust-vcprompt"
-  url "https://github.com/sscherfke/rust-vcprompt/archive/1.0.0.tar.gz"
-  sha256 "17afaaef3dcd4fd150f3fbc16a6d653a280ce09350c36d332b215a4756537354"
+  url "https://github.com/sscherfke/rust-vcprompt/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   head "https://github.com/sscherfke/rust-vcprompt.git"
 
   depends_on "rust" => :build
@@ -17,7 +17,7 @@ class RustVcprompt < Formula
     mkdir "repo" do
       system "git", "init"
       ENV.keys.grep(/^VCP_[A-Z]+/).each { |k| ENV.delete(k) }
-      assert_equal " ±\e[34mmaster\e[00m|\e[32m\e[01m✔\e[00m\n", shell_output("#{bin}/rust-vcprompt")
+      assert_equal " ±\x01\x1b[34mmain\x01\x1b[22;39m|\x01\x1b[33m↻1\x01\x1b[22;39m\n", shell_output("#{bin}/rust-vcprompt")
     end
   end
 end
